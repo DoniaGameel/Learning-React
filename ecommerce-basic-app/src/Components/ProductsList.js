eab09cbf16a5aa0a095756e0c2ff2d869ddbe03f
DoniaGameel <doniagameel34@gmail.com>
2024-09-07 17:48:22 +0300

    filtering

--- ecommerce-basic-app/src/Components/ProductsList.js
+++ ecommerce-basic-app/src/Components/ProductsList.js
@@ -5,16 +5,57 @@ function ProductsList() {
   const api_url = "https://fakestoreapi.com/products";
   // state
   let [products, setProducts] = useState([]);
+  let [categories, setCategories] = useState([]);
   // fetch the data
   useEffect(() => {
+    getProducts();
+    getCategories();
+  }, []);
+
+  const getProducts = () => {
     fetch(api_url)
       .then((res) => res.json())
       .then((data) => setProducts(data));
-  }, []);
+  };
+
+  const getCategories = () => {
+    fetch(`${api_url}/categories`)
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  };
+
+  const getProductsInCategory = (category) => {
+    fetch(`${api_url}/category/${category}`)
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  };
   return (
     <>
       <h2 className="text-center p-3">Our Products</h2>
       <div className="container">
+        {/* Button To show all products without filtering */}
+        <button
+          className="btn btn-info"
+          onClick={() => {
+            getProducts();
+          }}
+        >
+          All
+        </button>
+        {/* Categories Buttons */}
+        {categories.map((category) => {
+          return (
+            <button
+              key={category}
+              className="btn btn-info"
+              onClick={() => {
+                getProductsInCategory(category);
+              }}
+            >
+              {category}
+            </button>
+          );
+        })}
         <div className="row">
           {products.map((product) => {
             return (
