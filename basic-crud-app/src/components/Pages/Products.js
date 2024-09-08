ae78909f07915172cd84fb20acbdacf219260bb1
DoniaGameel <doniagameel34@gmail.com>
2024-09-08 10:43:23 +0300

    fetch and show the products data

--- basic-crud-app/src/components/Pages/Products.js
+++ basic-crud-app/src/components/Pages/Products.js
@@ -1,43 +1,48 @@
-import "./Products.css";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import "./Products.css";
 function Products() {
+  let [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:9000/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
   return (
     <>
       <h1>Products Page</h1>
       <Link to="/products/add" className="btn btn-success mt-3">
         Add New Product
       </Link>
-      <table class="table table-striped mt-5">
+      <table className="table table-striped mt-5 ml-5 mr-5">
         <thead>
           <tr>
             <th>ID</th>
             <th>Title</th>
+            <th>Description</th>
             <th>Price</th>
             <th>Operations</th>
           </tr>
         </thead>
 
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Product-1</td>
-            <td>100$</td>
-            <th>
-              <button className="btn btn-danger">Delete</button>
-              <button className="btn btn-info">View</button>
-              <button className="btn btn-primary">Edit</button>
-            </th>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Product-2</td>
-            <td>200$</td>
-            <th>
-              <button className="btn btn-danger">Delete</button>
-              <button className="btn btn-info">View</button>
-              <button className="btn btn-primary">Edit</button>
-            </th>
-          </tr>
+          {products.map((product) => {
+            return (
+              <tr key={product.id}>
+                <td>{product.id}</td>
+                <td className="title-td">{product.title}</td>
+                <td className="description-td">
+                  {product.description.slice(0, 100)}...
+                </td>
+                <td>{product.price}$</td>
+                <th>
+                  <button className="btn btn-danger">Delete</button>
+                  <button className="btn btn-info">View</button>
+                  <button className="btn btn-primary">Edit</button>
+                </th>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </>
