d83bf0709afe5ba8629b2282b131c5b275ec7fb9
DoniaGameel <doniagameel34@gmail.com>
2024-09-09 14:53:27 +0300

    init the project, the context and styling

---
+++ booklist/src/contexts/BookContext.js
@@ -0,0 +1,30 @@
+import React, { createContext, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+export const BookContext = createContext();
+
+const BookContextProvider = (props) => {
+  let [books, setBooks] = useState([
+    { title: "name of the wind", author: "patrick rothfuss", id: 1 },
+    { title: "the final empire", author: "brandon sanderson", id: 2 },
+  ]);
+
+  const addBook = (title, author) => {
+    setBooks([...books, { title, author, id: uuidv4() }]);
+  };
+
+  const removeBooks = (id) => {
+    setBooks(
+      books.filter((book) => {
+        return book.id !== id;
+      })
+    );
+  };
+  return (
+    <BookContext.Provider value={{ books, addBook, removeBooks }}>
+      {props.children}
+    </BookContext.Provider>
+  );
+};
+
+export default BookContextProvider;
