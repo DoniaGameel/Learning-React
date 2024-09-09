aee4ecee5299323db9f05080235d445bafc86162
DoniaGameel <doniagameel34@gmail.com>
2024-09-09 15:45:15 +0300

    New Book Form added

---
+++ booklist/src/components/BookForm.js
@@ -0,0 +1,42 @@
+import React, { useContext, useState } from "react";
+import { BookContext } from "../contexts/BookContext";
+
+const NewBookForm = () => {
+  let { addBook } = useContext(BookContext);
+  const [author, setAutor] = useState("");
+  const [title, setTitle] = useState("");
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        addBook(title, author);
+        setTitle("");
+        setAutor("");
+      }}
+    >
+      <input
+        required
+        type="text"
+        placeholder="book title"
+        value={title}
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+      />
+
+      <input
+        required
+        type="text"
+        placeholder="book author"
+        value={author}
+        onChange={(e) => {
+          setAutor(e.target.value);
+        }}
+      />
+
+      <input type="submit" value={"Add Book"} />
+    </form>
+  );
+};
+
+export default NewBookForm;
